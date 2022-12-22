762589595507aa3e9f14505851e50b097e07cb03
benessilfie <bquansah007@gmail.com>
2022-12-22 22:31:13 +0000

    Remove game object creation from game.rb

--- game.rb
+++ game.rb
@@ -51,16 +51,3 @@ class Game
     false
   end
 end
-
-# Create a new game
-
-# Create player objects
-player1 = Player.new('Player 1', 'X')
-player2 = Player.new('Player 2', 'O')
-
-# Create a board object
-board = Board.new
-
-# Create a new game and start it
-game = Game.new(player1, player2, board)
-game.play
